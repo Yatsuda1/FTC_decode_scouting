c72be187535d589e2547df7defacaf929ef29acc
Evan M <130808389+Yatsuda1@users.noreply.github.com>
2025-10-16 12:01:05 -0400

    Delete Project-Indigo-main directory

--- Project-Indigo-main/2024/homepage.js
+++
@@ -1,7 +0,0 @@
-var config_data = `
-{
-  "dataFormat": "tsv",
-  "title": "Reefscape Scouting 2025",
-  "page_title": "Reefscape",
-  "checkboxAs": "10",
-}`;
--- Project-Indigo-main/README.md
+++
@@ -1,2 +0,0 @@
-# Project-Indigo
- A custom scouting solution for team 5505 made from PWNAGE 2020 ScoutingPASS
