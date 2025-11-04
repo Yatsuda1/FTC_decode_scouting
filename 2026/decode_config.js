09f3e073a3f1b142417351cfd9dc62db58745d25
Evan M <130808389+Yatsuda1@users.noreply.github.com>
2025-11-04 13:07:35 -0500

    Update decode_config.js

--- 2026/decode_config.js
+++ 2026/decode_config.js
@@ -1,133 +1,131 @@
 var config_data = `
 {
-    "dataFormat": "tsv",
-    "title": "Decode Scouting 2025",
-    "page_title": "DECODE",
-    "checkboxAs": "10",
-    "prematch": [
-        {
-            "name": "Scouter Initials",
-            "code": "sct",
-            "type": "scouter",
-            "size": 5,
-            "maxSize": 5,
-            "required": "true"
-        },
-        {
-            "name": "Match Level",
-            "code": "lev",
-            "type": "level",
-            "choices": {
-                "qm": "Quals<br>",
-                "sf": "Semifinals<br>",
-                "f": "Finals"
-            },
-            "defaultValue": "qm",
-            "required": "true"
-        },
-        {
-            "name": "Robot Position",
-            "code": "pos",
-            "type": "level",
-            "choices": {
-                "b1": "Blue 1",
-                "b2": "Blue 2",
-                "r1": "Red 1",
-                "r2": "Red 2"
-            },
-            "required": "true"
-        },
-        {
-            "name": "Team #",
-            "code": "t",
-            "type": "team",
-            "min": 1,
-            "max": 999999,
-            "required": "true"
-        },
-        {
-            "name": "Auto Start Position",
-            "code": "as",
-            "type": "clickable_image",
-            "filename": "2026/field_image.png",
-            "clickRestriction": "one",
-            "allowableResponses": "13 14 15 25 26 27 28 29 30 35 36 37 38 39 40 41 42 47 48 49 50 51",
-            "shape": "circle 5 black red true",
-            "required": "true"
-        }
-    ],
-    "auton": [
-        {
-            "name": "Leave Starting Zone",
-            "code": "al",
-            "type": "bool"
-        },
-        {
-            "name": "Purple Artifacts Scored",
-            "code": "ac4",
-            "type": "counter"
-        },
-        {
-            "name": "Green Artifacts Scored",
-            "code": "ac3",
-            "type": "counter"
-        }
-    ],
-    "teleop": [
-        {
-            "name": "Purple Artifacts Scored",
-            "code": "ac4",
-            "type": "counter"
-        },
-        {
-            "name": "Green Artifacts Scored",
-            "code": "ac3",
-            "type": "counter"
-        },
-        {
-            "name": "Defence Timer",
-            "code": "eb",
-            "type": "timer"
-        }
-    ],
-    "endgame": [
-        {
-            "name": "Endgame Timer",
-            "code": "eb",
-            "type": "timer"
-        },
-        {
-            "name": "Final Status",
-            "code": "fs",
-            "type": "radio",
-            "choices": {
-                "np": "No Park<br>",
-                "xp": "Solo Park<br>",
-                "sf": "Solo Fail<br>",
-                "dp": "Double Park<br>",
-                "bf": "Double Fail<br>",
-                "NA": "Not Attempted<br>"
-            },
-            "defaultValue": "NA"
-        }
-    ],
-    "postmatch": [
-        {
-            "name": "Died/Immobilized",
-            "code": "die",
-            "type": "bool"
-        },
-        {
-            "name": "Tippy<br>(almost tipped over)",
-            "code": "tip",
-            "type": "bool"
-        },
-        {
-            "name": "Comments",
-            "code": "co",
-            "type": "text",
-            "size": 15,
-            "maxSize": 150
-        }
-    ]
+  "dataFormat": "tsv",
+  "title": "Decode Scouting 2025",
+  "page_title": "DECODE",
+  "checkboxAs": "10",
+  "prematch": [
+    { "name": "Scouter Initials",
+      "code": "s",
+      "type": "scouter",
+      "size": 5,
+      "maxSize": 5,
+      "required": "true"
+    },
+    { "name": "Event",
+      "code": "e",
+      "type": "event",
+      "defaultValue": "2025micmp2",
+      "required": "true"
+    },
+    { "name": "Match Level",
+      "code": "l",
+      "type": "level",
+      "choices": {
+        "qm": "Quals<br>",
+        "sf": "Semifinals<br>",
+        "f": "Finals"
+      },
+      "defaultValue": "qm",
+      "required": "true"
+    },
+    { "name": "Match #",
+      "code": "m",
+      "type": "match",
+      "min": 1,
+      "max": 150,
+      "required": "true"
+    },
+    { "name": "Robot",
+      "code": "r",
+      "type": "robot",
+      "choices": {
+        "r1": "Red-1",
+        "b1": "Blue-1<br>",
+        "r2": "Red-2",
+        "b2": "Blue-2<br>",
+      },
+      "required":"true"
+    },
+    { "name": "Team #",
+      "code": "t",
+      "type": "team",
+      "min": 1,
+      "max": 99999
+    },
+    { "name": "Auto Start Position",
+      "code": "as",
+      "type": "clickable_image",
+      "filename": "2024/field_image.png",
+      "clickRestriction": "one",
+      "allowableResponses": "6 7 18 19 30 31 42 43 54 55 66 67",
+      "shape": "circle 5 black red true"
+    }
+  ],
+  "auton": [
+    { "name": "Leave Starting Zone",
+      "code": "al",
+      "type": "bool"
+    },
+    { "name": "Rear Field Score",
+      "code": "afs",
+      "type": "counter"
+    },
+    { "name": "Front Field Score",
+      "code": "ars",
+      "type": "counter"
+    },
+    { "name": "Missed Score",
+      "code": "ams",
+      "type": "counter"
+    }
+  ],
+  "teleop": [
+    { "name": "Rear Field Score",
+      "code": "tfs",
+      "type": "counter"
+    },
+    { "name": "Front Field Score",
+      "code": "trs",
+      "type": "counter"
+    },
+    { "name": "Missed Score",
+      "code": "tms",
+      "type": "counter"
+    }
+],
+  "endgame": [
+    { "name": "Base Timer",
+      "code": "eb",
+      "type": "timer"
+    },
+    { "name": "Final Status",
+      "code": "es",
+      "type":"radio",
+      "choices": {
+        "sp": "Solo Park<br>",
+        "sf": "Failed Solo<br>",
+        "dp": "Double Park<br>",
+        "dp": "Failed Double<br>",
+        "na": "Not attempted"
+      },
+      "defaultValue": "na"
+    }
+  ],
+  "postmatch": [
+    { "name": "Died/Immobilized",
+      "code": "die",
+      "type": "bool"
+    },
+    { "name": "Tippy<br>(almost tipped over)",
+      "code": "tip",
+      "type": "bool"
+    },
+    { "name": "Comments",
+      "code": "co",
+      "type": "text",
+      "size": 15,
+      "maxSize": 150
+    }
+  ]
 }`;
